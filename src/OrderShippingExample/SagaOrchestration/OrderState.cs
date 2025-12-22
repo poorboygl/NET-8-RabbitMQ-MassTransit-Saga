110dc9381df9641e7e96a738a8e73c5711cdfa7b
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-12-22 23:09:19 +0700

    ch11-3-Set Up Saga Project

---
+++ src/OrderShippingExample/SagaOrchestration/OrderState.cs
@@ -0,0 +1,11 @@
+﻿using MassTransit;
+
+namespace SagaOrchestration;
+
+public class OrderState : SagaStateMachineInstance
+{
+    public Guid CorrelationId { get ; set ; }
+    public string CurrentState { get; set; } = string.Empty;
+    public Guid OrderId { get; set; }
+    public int  Quantity { get; set; }
+}
