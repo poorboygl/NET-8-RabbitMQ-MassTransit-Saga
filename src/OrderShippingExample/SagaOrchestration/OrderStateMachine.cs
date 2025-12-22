237715a86fd9efb95cede8c660555bb0f04236ab
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-12-22 23:57:57 +0700

    ch11-4-Set up OrderStateMachine

---
+++ src/OrderShippingExample/SagaOrchestration/OrderStateMachine.cs
@@ -0,0 +1,16 @@
+﻿using MassTransit;
+using static SharedMessages.Messages;
+
+namespace SagaOrchestration;
+
+public class OrderStateMachine : MassTransitStateMachine<OrderState>
+{
+    public State Submitted { get; private set; }
+    public State InventoryReserved { get; private set; }
+    public State PaymentCompleted { get; private set; }
+
+    public Event<OrderPlaced> OrderPlacedEvent { get; private set; }
+    public Event<InventoryReserved> InventoryReservedEvent { get; private set; }
+    public Event<PaymentCompleted> PaymentCompletedEvent { get; private set; }
+
+}
